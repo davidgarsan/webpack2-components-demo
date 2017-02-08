fe3b2df1f153c0cf48ffb8d3419a8303d1c70b78
David García Sánchez <davidgarsan@gmail.com>
2017-02-08 08:06:58 +0100

    REFACTOR improve performance

--- src/components/card/component.js
+++ src/components/card/component.js
@@ -21,10 +21,17 @@ function render({ title = 'Cargando', list, btnLabel = 'Aceptar', callback }, pa
   container.append(element);
   element.querySelector('button').onclick = callback;
   return element;
-} 
+}
 
 function update(element, { when, icon, desc, temp, hum }) {
-  element.querySelector('header img').src = `http://openweathermap.org/img/w/${icon}.png`;
+  if (icon) {
+    element.querySelector('header img').src = `http://openweathermap.org/img/w/${icon}.png`;
+    if (!element.classList.contains(styles.hasIcon)) {
+      element.classList.add(styles.hasIcon);
+    }
+  } else {
+    element.classList.remove(styles.hasIcon);
+  }
   element.querySelector('.when').innerHTML = when;
   element.querySelector('.desc').innerHTML = desc;
   element.querySelector('.temp').innerHTML = temp;
